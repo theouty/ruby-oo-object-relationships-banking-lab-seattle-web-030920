c5738a23d5226d89cf920d55e8480efeb26b93a6
Aman Michael <aman.david.michael@gmail.com>
2020-03-16 00:52:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,6 +33,8 @@ class Transfer
      sender.balance = sender.balance - amount 
      receiver.balance = receiver.balance + amount
      self.status = "complete"
+   else 
+     self.status = "rejected"
    end 
  end 
      
