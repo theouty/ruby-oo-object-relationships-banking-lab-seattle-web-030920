60c92005f9457b0fafac36f5248a2d847a4b783d
Aman Michael <aman.david.michael@gmail.com>
2020-03-16 00:18:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_accessor :sender, :receiver, :balance, :status
+  attr_reader  :name
+  
+  def initialize(name)
+    @name = name.freeze
+    @balance = 1000
+    @status = "open"
+    end
+  
+  def deposit(amount)
+    @balance = @balance + amount
+    end
+  
+  def display_balance 
+   "Your balance is $#{@balance}."
+  end 
+  
+  def valid? 
+    if @status == "open" && @balance > 0
+      true
+    else 
+      false
+    end 
+  end 
+  
+  def close_account
+    @balance = 0
+    @status = "closed"
+  end 
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+  
+  def valid?
+    if sender.valid? && receiver.valid? 
+      true 
+    else 
+      false
+    end 
+  end
+  
+  def execute_transaction
+    if (@status == 'pending') && valid?
+      @sender.deposit(-amount)
+      @receiver.deposit(amount)
+      @status = 'complete'
+    elsif !valid?
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+    
 end
